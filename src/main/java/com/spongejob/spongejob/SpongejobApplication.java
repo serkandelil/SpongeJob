8df94c2083163128d7e23cd5308b725b857446b9
serkan delil <47603816+serkandelil@users.noreply.github.com>
2020-12-01 00:51:34 +0300

    First commit

---
+++ src/main/java/com/spongejob/spongejob/SpongejobApplication.java
@@ -0,0 +1,13 @@
+package com.spongejob.spongejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpongejobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpongejobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spongejob/spongejob/SpongejobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spongejob.spongejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpongejobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
